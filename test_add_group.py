bd75304660c2d83721b284bc510141d4b593492c
tryzhova <tkotova04@gmail.com>
2018-05-13 22:07:03 -0400

    Scenario is updated to run on Firefox ESR

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+
 
 def is_alert_present(wd):
     try:
@@ -10,9 +10,10 @@ def is_alert_present(wd):
     except:
         return False
 
+
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
     def test_test_add_group(self):
@@ -44,6 +45,7 @@ class test_add_group(unittest.TestCase):
     
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == '__main__':
     unittest.main()

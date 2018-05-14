1a1c413ae41b822d9096143507003c66914436df
tryzhova <tkotova04@gmail.com>
2018-05-13 23:22:04 -0400

    added test for adding empty contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -14,7 +14,25 @@ class test_add_contact(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_contact_page(wd)
-        self.create_new_contact(wd)
+        self.create_new_contact(wd, name="Tatiana", midname="V", lastname="Kotova", nickname="TKotova",
+                                title="Quality Assurance Engineer", company="RS",
+                                address="Boston, MA", home="123", mobile="456", work="789", fax="10",
+                                email1="email1@tk.com", email2="email2@tk.com", email3="email3.tk.com",
+                                homepage="homepage.xom", day=6, month=5, year="1984", aday=17, amonth=7,
+                                ayear="2006", address2="address2", phone2="321", notes="notes")
+        self.logout(wd)
+    def test_test_add_empty_contact(self):
+
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_contact_page(wd)
+        self.create_new_contact(wd, name="", midname="", lastname="", nickname="",
+                                title="", company="",
+                                address="", home="", mobile="", work="", fax="",
+                                email1="", email2="", email3="",
+                                homepage="", day=6, month=5, year="", aday=17, amonth=7,
+                                ayear="", address2="", phone2="", notes="")
         self.logout(wd)
 
     def login(self, wd, username, password):
@@ -30,11 +48,9 @@ class test_add_contact(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def create_new_contact(self, wd, name="Tatiana", midname="V", lastname="Kotova", nickname="TKotova",
-                           title="Quality Assurance Engineer", company="RS", address="Boston, MA", home="123", mobile="456",
-                           work="789", fax="10", email1="email1@tk.com", email2="email2@tk.com",
-                           email3="email3.tk.com", homepage="homepage.xom", day=6, month=5, year="1984", aday=17, amonth=7, ayear="2006",
-                           address2="address2", phone2="321", notes="notes"):
+    def create_new_contact(self, wd, name, midname, lastname, nickname, title, company, address, home, mobile, work,
+                           fax, email1, email2, email3, homepage, day, month, year, aday, amonth, ayear, address2,
+                           phone2, notes):
         # fill in contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()

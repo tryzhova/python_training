5a0c44e04db2f8e2ca4e3ca5769ed3ad3f6fb035
tryzhova <tkotova04@gmail.com>
2018-05-14 22:15:32 -0400

    Created conftest.py

--- fixture/group.py
+++ fixture/group.py
@@ -25,4 +25,11 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        self.open_groups_page()
+
 
